d53fb0088ad7c426224f0da3debf9f666f615489
Antti Vähäkotamäki <antti@dicole.com>
2014-04-15 16:45:01 +0300

    amv specific vagrant mappings

---
+++ Vagrantfile.d/amv.rb
@@ -0,0 +1,12 @@
+# Vagrantfile.d/default.rb
+# vi: set sw=2 ts=2 sts=2 ft=ruby :
+#
+# This file is loaded when and only when
+# Vagrantfile.d/USERNAME.rb is not found.
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+    config.vm.network "forwarded_port", guest: 8088, host: 12080
+    config.vm.network "forwarded_port", guest: 443,  host: 12443
+    config.vm.synced_folder "/Users/amv/git/dcp", "/opt/meetings/dcp"
+    config.vm.synced_folder "/Users/amv/git/data-api-node", "/opt/meetings/data-api"
+end
