1ab80f61492ea9dd6d5cb0fa6d013893207ec55d
Antti Vähäkotamäki <antti@dicole.com>
2014-04-15 16:43:14 +0300

    added demo user as part of provisioning

--- sql/values.sql
+++ sql/values.sql
@@ -4,7 +4,7 @@ INSERT INTO dicole_theme VALUES (null, 'dicole_only_theme', '0.01', '', '', '', 
 
 INSERT INTO dicole_domain VALUES (1, 'local.meetin.gs', null, 1, 'en', 'Europe/Helsinki', '', 'meetings_navigation', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0);
 
-INSERT INTO dicole_url_alias VALUES (97, 1, 0, 0, 0, 'meetings', 'meet', 'meet');
+INSERT INTO dicole_url_alias VALUES (1, 1, 0, 0, 0, 'meetings', 'meet', 'meet');
 
 INSERT INTO dicole_tool_settings VALUES (null, 0, 0, 'domain_user_manager_1', 'alternative_login', '/meetings/login');
 INSERT INTO dicole_tool_settings VALUES (null, 0, 0, 'domain_user_manager_1', 'front_page', '/meetings_global/detect/');
@@ -12,3 +12,7 @@ INSERT INTO dicole_tool_settings VALUES (null, 0, 0, 'domain_user_manager_1', 'c
 INSERT INTO dicole_tool_settings VALUES (null, 0, 0, 'domain_user_manager_1', 'not_found_template', 'dicole_meetings::main_meetings_404');
 INSERT INTO dicole_tool_settings VALUES (null, 0, 0, 'domain_user_manager_1', 'not_found_action', 'meetings_404');
 INSERT INTO dicole_tool_settings VALUES (null, 0, 0, 'domain_user_manager_1', 'domain_notify_email', '"Test Meetin.gs" <testinfo@test.meetin.gs>');
+
+INSERT INTO sys_user VALUES (1,'','IKXQR/b03UN1w','demo@meetin.gs',NULL,NULL,NULL,NULL,1,'Demo',NULL,'User','Demo User',NULL,'en','ctIyRBdwnYmZnMw6','{"1":{"creation_time":1}}',NULL,'Europe/Helsinki',NULL,NULL,NULL,0,NULL,NULL,NULL);
+INSERT INTO dicole_domain_user VALUES (1,1,1);
+
