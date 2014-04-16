27185a8817f8f4dc40c40352ba162bfd6b68e537
Tuomas Starck <dev@starck.fi>
2014-04-16 21:01:45 +0300

    Fix hilights and some

--- Vagrantfile.d/amv.rb
+++ Vagrantfile.d/amv.rb
@@ -1,8 +1,4 @@
-# Vagrantfile.d/default.rb
-# vi: set sw=2 ts=2 sts=2 ft=ruby :
-#
-# This file is loaded when and only when
-# Vagrantfile.d/USERNAME.rb is not found.
+# Vagrantfile.d/amv.rb
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.network "forwarded_port", guest: 8088, host: 12080
