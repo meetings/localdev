3964aedb2ab6529446217e9ceacd4a5361715cde
Jussi Kaijalainen <fiddler@iki.fi>
2014-05-13 14:34:05 +0400

    Add masters masterful vagrant synced_file config

---
+++ Vagrantfile.d/master.rb
@@ -0,0 +1,8 @@
+# Vagrantfile.d/amv.rb
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+    config.vm.network "forwarded_port", guest: 8088, host: 12080
+    config.vm.network "forwarded_port", guest: 443,  host: 12443
+    config.vm.synced_folder "/Users/master/sites/dcp", "/opt/meetings/dcp"
+    config.vm.synced_folder "/Users/master/sites/data-api-node", "/opt/meetings/data-api"
+end
